4c52a2b1de8443299e721f867fb02d55158e4228
Shibata Hazuki <shibatahazuki@Shibata-no-MacBook-Pro.local>
2018-05-14 17:37:52 +0900

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="my-component">
+        <button @click= "onClick">{{message}}</button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .my-component {
+        color: #555;
+        button {
+            border: 1px solid #555;
+            font-size: 40px;
+        }
+    }
+</style>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: 'NewThread'
+            }
+        },
+        methods: {
+            onClick() {
+                console.log(this.message)
+            }
+        }
+    }
+</script>
